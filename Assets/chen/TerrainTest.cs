a6eccb02713ccae8a8565143896fa2bf9b6726d2
Lind Chen <cozlind@live.com>
2017-01-21 05:05:50 +0900

    no message

--- Assets/chen/TerrainTest.cs
+++ Assets/chen/TerrainTest.cs
@@ -23,7 +23,6 @@ public class TerrainTest : MonoBehaviour {
         if (Time.time - time < updateTime) return;
         time = updateTime;
 
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Input.GetMouseButton(0)&&Physics.Raycast(ray,out hit)&&hit.collider!=null)
