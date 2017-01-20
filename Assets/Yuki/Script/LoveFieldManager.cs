0d7c7f51a2c46943ecf6c4fa8a08435c1148db14
増山 <analogwatch24@gmail.com>
2017-01-21 01:38:14 +0900

    [追加]自分のフォルダ内のScriptフォルダにLove波による増殖のスクリプトを作成

---
+++ Assets/Yuki/Script/LoveFieldManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoveFieldManager : MonoBehaviour {
+    public float countTime;
+    private float time;
+    private int humanCount;
+    private int makeChildCount;
+    public float scale;
+    public GameObject human;
+    private bool countFinished = false;
+    private bool makingChild = false;
+	// Use this for initialization
+	void Start () {
+        time = countTime;
+        transform.localScale = new Vector3(scale, scale, scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+
+        if (countFinished == true && makingChild == false)
+        {
+            MakeChildren();
+            makingChild = true;
+        }
+
+        if (time <= 0) Destroy(gameObject);
+	}
+
+    void MakeChildren()
+    {
+        for (int i = 0; i < makeChildCount; i++)
+        {
+            Instantiate(human, new Vector3(transform.position.x ,transform.position.y + 3,transform.position.z), transform.rotation);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (countFinished == false)
+        {
+            GameObject[] targets = GameObject.FindGameObjectsWithTag("Human");
+            foreach (GameObject obj in targets)
+            {
+                float dist = Vector3.Distance(obj.transform.position, transform.position);
+                if (dist <= scale / 2)
+                {
+                    humanCount++;
+                }
+            }
+
+            makeChildCount = (int)Mathf.Floor(humanCount / 2);
+            countFinished = true;
+        }
+    }
+}
