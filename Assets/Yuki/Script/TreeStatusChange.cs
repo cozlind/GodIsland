ed41515efda9e0b96ed768720e5b7e513aa67a8c
増山 <analogwatch24@gmail.com>
2017-01-21 09:49:55 +0900

    [追加]地面をクリックすると木が生えてくるスクリプトとテスト用シーンの追加

---
+++ Assets/Yuki/Script/TreeStatusChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeStatusChange : MonoBehaviour {
+    public float growTime = 3.0f;
+    private float time;
+    public Vector3 grownTreeSize;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = growTime;
+        grownTreeSize = transform.localScale;
+        
+        transform.localScale = new Vector3(0.00001f, 0.00001f, 0.00001f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Growing();
+    }
+
+    void Growing()
+    {
+        if (time >= 0)
+        {
+            transform.localScale += (grownTreeSize / growTime) * Time.deltaTime;
+            time -= Time.deltaTime;
+        }
+    }
+}
