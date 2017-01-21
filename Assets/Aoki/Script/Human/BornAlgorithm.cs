f559ecc0808ab4f542680637e3175a386cbe9658
shingoaoki <sp4blue@gmail.com>
2017-01-21 10:35:46 +0900

    ステータスに攻撃とＨＰ、生成できるか確認するステータス追加　生成するときに強さの係数を入れているが失敗している感じ・・

--- Assets/Aoki/Script/Human/BornAlgorithm.cs
+++ Assets/Aoki/Script/Human/BornAlgorithm.cs
@@ -6,21 +6,29 @@ public class BornAlgorithm : MonoBehaviour {
 
     //[SerializeField]
     //List<HumanStatus> humans;
-
-
+    public static Color GetBornColor(Color color1, Color color2)
+    {
+        Color color = Color.white;
+        color = color1 + color2;
+        color.r = Mathf.Clamp(color.r, 0, 1);
+        color.g = Mathf.Clamp(color.g, 0, 1);
+        color.b = Mathf.Clamp(color.b, 0, 1);
+        return color;
+    }
 
     public static HumanStatus GetBornAddStatus( HumanStatus status1, HumanStatus status2 )
     {
         HumanStatus humanStatus_ = new HumanStatus();
 
         Color color_ = Color.white ;
-        color_ = status1.color + status2.color;
-        color_.r = Mathf.Clamp(color_.r, 0, 1);
-        color_.g = Mathf.Clamp(color_.g, 0, 1);
-        color_.b = Mathf.Clamp(color_.b, 0, 1);
+        color_ = GetBornColor( status1.color, status2.color );
 
         humanStatus_.color = color_;
-        
+
+        humanStatus_.attack = HumanStatusAlgorithm.GetAttackPersent( humanStatus_.color );
+        humanStatus_.hp = HumanStatusAlgorithm.GetBornHpPersent( humanStatus_.color );
+
+
         return humanStatus_;
     }
 
