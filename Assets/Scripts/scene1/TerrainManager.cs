8978b3ef504545752642bde55799824f4b76ce6e
Lind Chen <cozlind@live.com>
2017-01-21 12:22:04 +0900

    no message

--- Assets/Scripts/scene1/TerrainManager.cs
+++ Assets/Scripts/scene1/TerrainManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainTest : MonoBehaviour {
+public class TerrainManager : MonoBehaviour {
 
     Terrain terrain;
     TerrainData terrainData;
---
+++ Assets/Scripts/startscene/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject panel_quit;
+    void Start()
+    {
+        panel_quit.SetActive(false);
+    }
+	public void StartGame()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void OpenQuitPanel()
+    {
+        panel_quit.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void CancelQuit()
+    {
+        panel_quit.SetActive(false);
+    }
+}
