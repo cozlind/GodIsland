f5a72ac16695f54122fec71e901ce28ac337e6e0
Hiroki Yamazaki <m0114454e8@edu.teu.ac.jp>
2017-01-22 01:10:11 +0900

    UI枠追加

---
+++ Assets/yamazaki/Script/UIwaku.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIwaku : MonoBehaviour {
+
+    public RectTransform[] _RT;
+
+    public void BUTTON0()
+    {
+        transform.position = _RT[0].position;
+    }
+
+    public void BUTTON1()
+    {
+        transform.position = _RT[1].position;
+    }
+
+    public void BUTTON2()
+    {
+        transform.position = _RT[2].position;
+    }
+
+    public void BUTTON3()
+    {
+        transform.position = _RT[3].position;
+    }
+}
