89e33dc9b7949ea7adaf8cff2870ab884997ceaf
Hiroki Yamazaki <m0114454e8@edu.teu.ac.jp>
2017-01-22 11:48:47 +0900

    BGMスクリプト変更

---
+++ Assets/yamazaki/Script/RangeBGM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeBGM : MonoBehaviour {
+
+    public GameObject _Giant;
+    public bool _BGM_flag = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_BGM_flag == false)
+        {
+            transform.position = _Giant.transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("human"))
+        {
+            _BGM_flag = true;
+        }
+    }
+}
